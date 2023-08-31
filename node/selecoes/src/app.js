704ca3704a3df533a75472d678c88ab8458f71f0
Eduardo J.S <103137675+Eduardo-J-S@users.noreply.github.com>
2023-08-31 18:56:14 -0300

    node: finalizando o projeto seleções

--- node/selecoes/src/app.js
+++ node/selecoes/src/app.js
@@ -1,31 +1,12 @@
 import express from 'express';
-import SelecaoController from './app/controllers/selecaoController.js'
+import routes from './routes.js'
 
 const app = express();
 
 // indicar para para o express ler body com json
 app.use(express.json());
 
-// retornar o objeto por id
-function buscarSelecaoPorId(id){
-    return selecoes.filter(selecao => selecao.id == id)
-}
-
-// pegar a posição ou index do elemendo no array por id
-function buscarIndexSelecao(id){
-    return selecoes.findIndex(selecao => selecao.id == id)
-}
-
-// ROTAS
-app.get('/selecoes', SelecaoController.index)
-
-app.get('/selecoes/:id', SelecaoController.show)
-
-app.post('/selecoes', SelecaoController.store)
-
-app.put('/selecoes/:id', SelecaoController.update)
-
-app.delete('/selecoes/:id', SelecaoController.delete)
-
+// usar o router
+app.use(routes)
 
 export default app;
--- node/selecoes/src/app/controllers/SelecaoController.js
+++ node/selecoes/src/app/controllers/SelecaoController.js
@@ -1,77 +1,36 @@
-import conexao from "../database/conexao.js"
+import SelecaoRepository from "../repositories/SelecaoRepository.js"
 
 class SelecaoController {
 
-    index(req, res) {
-        //res.status(200).send(selecoes)
-        const sql = "SELECT * FROM selecoes;"
-        conexao.query(sql, (erro, resultado) => {
-            if (erro) {
-                res.status(404).json({ 'erro': 'Dados não localizados ' + erro })
-            } else {
-                res.status(200).json(resultado)
-            }
-        })
+    async index(req, res) {
+
+        const row = await SelecaoRepository.findAll()
+        res.json(row)
     }
 
-    show(req, res) {
-        //res.json(buscarSelecaoPorId(req.params.id))
+    async show(req, res) {
         const id = req.params.id
-        const sql = "SELECT * FROM selecoes WHERE id=?;"
-        conexao.query(sql, id, (erro, resultado) => {
-            const linha = resultado[0]
-            if (erro) {
-                res.status(404).json({ 'erro': 'Dados não localizados ' + erro })
-            } else {
-                res.status(200).json(linha)
-            }
-        })
+        const row = await SelecaoRepository.findById(id);
+        res.json(row)
     }
     
-    store(req, res) {
-        //selecoes.push(req.body)
-        //res.status(201).send('Seleção cadastra com sucesso.')
+    async store(req, res) {
         const selecao = req.body
-        const sql = "INSERT INTO selecoes SET ?;"
-        conexao.query(sql, selecao, (erro, resultado) => {
-            if(erro){
-                res.status(404).json({'erro' : 'Dados não localizados ' + erro})
-            } else {
-                res.status(201).json(resultado)
-            }
-        })
+        const row = await SelecaoRepository.create(selecao);
+        res.json(row);
     }
 
-    update(req, res) {
-        // let index = buscarIndexSelecao(req.params.id)
-        // selecoes[index].selecao = req.body.selecao
-        // selecoes[index].grupo = req.body.grupo
-        // res.send(selecoes)
+    async update(req, res) {
         const selecao = req.body
         const id = req.params.id
-        const sql = "UPDATE selecoes SET ? WHERE id=?;"
-        conexao.query(sql, [selecao, id], (erro, resultado) => {
-            if(erro){
-                res.status(404).json({'erro' : 'Dados não localizados ' + erro})
-            } else {
-                res.status(200).json(resultado)
-            }
-        })
+        const row = await SelecaoRepository.update(selecao, id);
+        res.json(row);
     }
 
-    delete(req, res) {
-        // let index = buscarIndexSelecao(req.params.id)
-        // selecoes.splice(index, 1)
-        // res.send(`Seleção com o id ${req.params.id} excluída com sucesso`)
+    async delete(req, res) {
         const id = req.params.id
-        const sql = "DELETE FROM selecoes WHERE id=?;"
-        conexao.query(sql, id, (erro, resultado) => {
-            if(erro){
-                res.status(404).json({'erro' : 'Dados não localizados ' + erro})
-            } else {
-                res.status(200).json(resultado)
-            }
-        })
+        const row = await SelecaoRepository.delete(id);
+        res.json(row);
     }
 }
 

e4a49265bf289f2215fa286e76536407c61d4f09
Eduardo J.S <103137675+Eduardo-J-S@users.noreply.github.com>
2023-09-01 19:45:42 -0300

    segundo projeto spring boot
    criando uma página web com um crud simples para treinar

---
+++ spring/mensagemWeb/src/test/java/com/eduardojs/mensagemWeb/MensagemWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eduardojs.mensagemWeb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MensagemWebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

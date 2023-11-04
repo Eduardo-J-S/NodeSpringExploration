b7119503106f375f884ad0482a6b22f55c03a2e6
Eduardo J.S <103137675+Eduardo-J-S@users.noreply.github.com>
2023-11-03 21:32:12 -0300

    projetinho spring

---
+++ spring/erudio5/src/main/resources/db/migration/V2__Populate_Table_Person.sql
@@ -0,0 +1,6 @@
+INSERT INTO `person` (`id`, `address`, `first_name`, `gender`, `last_name`) VALUES
+	(1, 'São Paulo', 'Ayrton', 'Male', 'Senna'),
+	(3, 'Smiljan - Croatia', 'Nikola', 'Male', 'Tesla'),
+	(4, 'Smiljan - Croatia', 'Nikola', 'Male', 'Tesla'),
+	(5, 'Smiljan - Croatia', 'Nikola', 'Male', 'Tesla');
+
